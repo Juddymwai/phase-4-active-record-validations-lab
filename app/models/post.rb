227688d1ca5dac1247725b03022998c3a44936f8
Judy Mwai <juddiemukami@gmail.com>
2022-10-03 15:56:07 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]} 
+
+    # validate :title, inclusion{in: ["Won't Believe","Secret","Top [number]","Guess"]}
+    custom_patterns =[
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if custom_patterns.none? {|cus| cus.match title}
+        error.add(:title, "must be clickbait")
+        end
+    end 
 end
